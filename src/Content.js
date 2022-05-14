7d1dd81802e0d3e1327b1d56e3f82e52684be905
gabs <gabriellerrc@gmail.com>
2022-05-14 16:40:34 -0300

    footer responsivo

--- src/Content.js
+++ src/Content.js
@@ -7,7 +7,7 @@ export default function Content(){
         <div>
             <Header />
             <Play/>
-            <Footer/>
+            
         </div>
     )
 }
--- src/Play.js
+++ src/Play.js
@@ -3,15 +3,26 @@ import close from "./img/close.svg"
 import help from "./img/help.svg"
 import play from "./img/play.svg"
 import setinha from "./img/setinha.png"
+import party from "./img/party.png"
+import sad from "./img/sad.png"
 import React from 'react';
 
 export default function Play (){
     return(
-        <Cards />
+        <>
+            <Cards />
+        </>
     )
 }
 
+
 function Cards (){
+
+    const [rightCount, setRightCount] = React.useState(0);
+    const [footCount, setFootCount] = React.useState(0);
+    const [iconCount, setIconCount] = React.useState([]);
+
+
     const text = [{
         title: "Pergunta 1",
         question: "O que é JSX?",
@@ -58,23 +69,29 @@ function Cards (){
 
     return (
         <div className="allQuestions content">
-            {text.map(Card)}
+            {text.map(card => <Card title={card.title} question={card.question} answer={card.answer} footCount={footCount} setFootCount={setFootCount} rightCount={rightCount} setRightCount={setRightCount} iconCount={iconCount} setIconCount={setIconCount}/>)}
+            <Foot footCount={footCount} setFootCount={setFootCount} rightCount={rightCount} setRightCount={setRightCount} iconCount={iconCount} setIconCount={setIconCount}/>
         </div>
     )
 }
 
 
-function Card ({title, question, answer}){
+function Card ({title, question, answer, footCount, setFootCount, rightCount, setRightCount, iconCount, setIconCount}){
     const [classButton, setClassButton] = React.useState("play-button");
     const [classAnswer, setClassAnswer] = React.useState("")
     const [changeIcon, setChangeIcon] = React.useState(play)
     const [lockIcon, setLockIcon] = React.useState()
+
 
     function changeStatusRight(){
         setClassButton("play-button")
         setClassAnswer("right")
         setChangeIcon()
         setLockIcon(check)
+        setFootCount(footCount +1)
+        setRightCount(rightCount +1)
+        iconCount.push(<img src={check} alt=""/>)
+
     }
 
     function changeStatusAlmost(){
@@ -82,6 +99,10 @@ function Card ({title, question, answer}){
         setClassAnswer("partial")
         setChangeIcon()
         setLockIcon(help)
+        setFootCount(footCount +1)
+        setRightCount(rightCount +1)
+        iconCount.push(<img src={help} alt=""/>)
+
     }
 
     function changeStatusWrong(){
@@ -89,14 +110,16 @@ function Card ({title, question, answer}){
         setClassAnswer("wrong")
         setChangeIcon()
         setLockIcon(close)
+        setFootCount(footCount +1)
+        iconCount.push(<img src={close} alt=""/>)
     }
 
 
     if (classButton==="play-button"){
         return(
             <div className={classButton}> 
-                <img className="icon" src={changeIcon} onClick={() => setClassButton("question-button")}/><p className={classAnswer}>{title}</p>
-                <img className="icon" src={lockIcon}/>
+                <img className="icon" src={changeIcon} alt="" onClick={() => setClassButton("question-button")}/><p className={classAnswer}>{title}</p>
+                <img className="icon" src={lockIcon} alt=""/>
             </div>
         )
     }
@@ -104,11 +127,11 @@ function Card ({title, question, answer}){
     else if (classButton==="question-button"){
         return(
             <div className={classButton}> 
-                <img className="arrow" src={setinha} onClick={() => setClassButton("answer-button")}/>{question}
+                <img className="arrow" src={setinha} alt="" onClick={() => setClassButton("answer-button")}/>{question}
             </div>
         )
-
     }
+
     else if (classButton==="answer-button"){
         return(
             <div className={classButton}>
@@ -122,3 +145,43 @@ function Card ({title, question, answer}){
         )
     }
 }
+
+function Foot({footCount, rightCount, iconCount}){
+
+
+    if (footCount !== 8){
+        return(
+            <div className="footer-main">
+                <div className="footer-title"><img src="" alt=""/></div>
+                <div className="footer-text"></div>
+                <div>{footCount}/8 CONCLUÍDOS</div>
+                <div className="footer-icons">
+                    {iconCount}
+                </div>
+            </div>
+        )
+    } else if (footCount ===8 && rightCount !==8){
+        return(
+            <div className="footer-main">
+                <div className="footer-title"><img src={sad} alt=""/> Putz!</div>
+                <div className="footer-text">Ainda faltam alguns... Mas não desanime!</div>
+                <div>{footCount}/8 CONCLUÍDOS</div>
+                <div className="footer-icons">
+                    {iconCount}
+                </div>
+            </div>
+        )
+    } else if (footCount ===8 && rightCount ===8){
+        return(
+            <div className="footer-main">
+                <div className="footer-title"><img src={party} alt=""/> Parabéns!</div>
+                <div className="footer-text">Você não esqueceu de nenhum flashcard!</div>
+                <div>{footCount}/8 CONCLUÍDOS</div>
+                <div className="footer-icons">
+                    {iconCount}
+                </div>
+            </div>
+        )
+    }
+}
+
